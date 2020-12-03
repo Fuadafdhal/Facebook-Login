d12f70448b99d2cdde9ba2d7ba4bbd06443801c9
Afdhal_FA <fuad.afdal.fa@gmail.com>
2020-12-03 23:10:46 +0700

    initial commit

---
+++ app/src/main/java/com/afdha_fa/facebook_login/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.afdha_fa.facebook_login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.afdha_fa.facebook_login.databinding.ActivityMainBinding
+import com.facebook.*
+
+
+/*
+TODO: This lib penting
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+ */
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "MainActiviry"
+
+    private lateinit var auth: FirebaseAuth
+
+
+    var callbackManager = CallbackManager.Factory.create()
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+
+        //Initialise FacebookS DK
+        FacebookSdk.sdkInitialize(this)
+        // Initialize Facebook Login button
+        callbackManager = CallbackManager.Factory.create()
+
+        binding.buttonFacebookLogin.setReadPermissions("email", "public_profile")
+        binding.buttonFacebookLogin.registerCallback(callbackManager, object :
+            FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                Log.d(TAG, "facebook:onCancel")
+                // ...
+            }
+
+            override fun onError(error: FacebookException) {
+                Log.d(TAG, "facebook:onError", error)
+                // ...
+            }
+        })
+
+
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    // ...
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun updateUI(fUser: FirebaseUser?) {
+        if (fUser != null) {
+            startActivity(Intent(this, WelcomeActivity::class.java))
+
+        } else {
+            Toast.makeText(this, "Pleace Sign In On Facebook", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/afdha_fa/facebook_login/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package com.afdha_fa.facebook_login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_activiry)
+    }
+}
